35022d4df86257471f679f22f1faabe135a3aea9
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-06-20 19:41:13 -0600

    installed bootstrap

--- proyecto_prueba/proyecto/proyecto/settings.py
+++ proyecto_prueba/proyecto/proyecto/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "libros"
+    "libros",
+    "bootstrap4",
+    "widget_tweaks"
 ]
 
 MIDDLEWARE = [
