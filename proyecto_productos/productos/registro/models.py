5268531dd4cb0f31fa72ce363f20e91af9fadaa2
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-06-29 21:01:40 -0600

    created basic productos parameters

--- proyecto_productos/productos/registro/models.py
+++ proyecto_productos/productos/registro/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Productos(models.Model):
+    nombre = models.CharField(null=False)
+    descripcion = models.CharField(null=False)
+    precio = models.IntegerField(null=False)
+    fecha_de_registro = models.DateField(null=False)
+    estatus = models.CharField(null=False)
+
+    def __str__(self):
+        return self.nombre
