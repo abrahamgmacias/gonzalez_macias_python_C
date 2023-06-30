3a8911ccfd33bfe27050a7b30b038609f68298f0
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-06-29 21:04:03 -0600

    added new descriptions to productos parameters

--- proyecto_productos/productos/registro/models.py
+++ proyecto_productos/productos/registro/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Productos(models.Model):
-    nombre = models.CharField(null=False)
-    descripcion = models.CharField(null=False)
+    nombre = models.CharField(max_length=50, null=False)
+    descripcion = models.CharField(max_length=200, null=False)
     precio = models.IntegerField(null=False)
     fecha_de_registro = models.DateField(null=False)
-    estatus = models.CharField(null=False)
+    estatus = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return self.nombre
