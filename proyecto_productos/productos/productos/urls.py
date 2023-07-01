0950f5f434e093d69e93159762d77c8f30645449
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-07-01 17:38:48 -0600

    changed registros route name to productos

--- proyecto_productos/productos/productos/urls.py
+++ proyecto_productos/productos/productos/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("registro/", include("registro.urls"))
+    path("productos/", include("registro.urls"))
 ]
