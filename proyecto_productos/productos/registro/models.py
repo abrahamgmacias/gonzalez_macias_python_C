bed6d78d6025758bc973fbe6eaec9082a45595fe
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-07-01 16:55:20 -0600

    created registro_producto table

--- proyecto_productos/productos/registro/models.py
+++ proyecto_productos/productos/registro/models.py
@@ -6,7 +6,14 @@ class Producto(models.Model):
     descripcion = models.CharField(max_length=200, null=False)
     precio = models.IntegerField(null=False)
     fecha_de_registro = models.DateField(null=False)
-    estatus = models.CharField(max_length=100, null=False)
+    estatus = models.CharField(
+        max_length=100, 
+        null=False, 
+        choices=[
+            ('DISP', 'Disponible'), 
+            ('AGOT', 'Agotado')
+            ]
+        )
 
     def __str__(self):
         return self.nombre
